c8a09cdf83e9e21de7936689e1cfb8039599a6e5
nojinXyoshie <yoshietamvan@gmail.com>
2021-06-01 21:22:31 +0700

    create slider produk

--- app/src/main/java/com/example/ushop/adapter/AdapterSlider.kt
+++ app/src/main/java/com/example/ushop/adapter/AdapterSlider.kt
@@ -10,9 +10,6 @@ import androidx.viewpager.widget.PagerAdapter
 import com.example.ushop.R
 import java.util.ArrayList
 
-/**
- * Created by: Tisto
- */
 class AdapterSlider(var data: ArrayList<Int>, var context: Activity?) : PagerAdapter() {
     lateinit var layoutInflater: LayoutInflater
 
--- app/src/main/java/com/example/ushop/fragment/BerandaFragment.kt
+++ app/src/main/java/com/example/ushop/fragment/BerandaFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.ushop.R
+import com.example.ushop.adapter.AdapterProduk
 import com.example.ushop.adapter.AdapterSlider
+import com.example.ushop.model.Produk
 
 
 class BerandaFragment : Fragment() {
 
     lateinit var vpSlider : ViewPager
+    lateinit var rvProduk: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +26,8 @@ class BerandaFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_beranda, container, false)
 
-        vpSlider = view.findViewById(R.id.vp_slider);
+        vpSlider = view.findViewById(R.id.vp_slider)
+        rvProduk = view.findViewById(R.id.rv_produk)
 
         val arrSlider = ArrayList<Int>()
         arrSlider.add(R.drawable.slider1)
@@ -31,7 +37,38 @@ class BerandaFragment : Fragment() {
         val adapterSlider = AdapterSlider(arrSlider, activity)
         vpSlider.adapter = adapterSlider
 
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+        rvProduk.adapter = AdapterProduk(arrProduk)
+        rvProduk.layoutManager = layoutManager
+
         return view
+    }
+
+    val arrProduk: ArrayList<Produk>get() {
+        val arr = ArrayList<Produk>()
+
+        val p1 = Produk()
+        p1.nama = "Mito"
+        p1.harga = "Rp.5.000.000"
+        p1.gambar = R.drawable.slider3
+
+        val p2 = Produk()
+        p2.nama = "Xiaomi"
+        p2.harga = "Rp.5.000.000"
+        p2.gambar = R.drawable.slider2
+
+        val p3 = Produk()
+        p3.nama = "Cross"
+        p3.harga = "Rp.5.000.000"
+        p3.gambar = R.drawable.slider1
+
+        arr.add(p1)
+        arr.add(p2)
+        arr.add(p3)
+
+        return arr
     }
 
 }
